5085a364ea31965e91b7e87ba7ed0a5cef1bc494
Anton Skvartsou <antonskwr@gmail.com>
2020-10-08 18:49:16 +0200

    Corrections for PR comments

--- main.go
+++ main.go
@@ -9,11 +9,12 @@ import (
 
 func main() {
 	s := server.NewServer()
-	log.Printf("Starting server on port%s ...\n", port())
-	log.Fatal(s.ListenAndServe(port()))
+	port := getPort()
+	log.Printf("Starting server on port%s ...\n", port)
+	log.Fatal(s.ListenAndServe(port))
 }
 
-func port() string {
+func getPort() string {
 	port := os.Getenv("PORT")
 	if len(port) == 0 {
 		port = "8080"
