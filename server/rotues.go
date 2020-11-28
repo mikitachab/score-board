ff3db58e1f984a297e1b408c2090509acb74540e
Anton Skvartsou <antonskwr@gmail.com>
2020-11-28 20:54:09 +0100

    Implemented static file server

--- server/rotues.go
+++ server/rotues.go
@@ -1,6 +1,14 @@
 package server
 
+import "net/http"
+
 func (s *Server) setupRoutes() {
+	// Setup static file server
+	publicPath := "/public/"
+	pfs := http.StripPrefix(publicPath, getPublicFileServer())
+	s.mux.PathPrefix(publicPath).Handler(pfs).Methods("GET")
+
+	// Setup handlers
 	s.Handle("/players", "players_list.html", handlePlayersList).Methods("GET")
 	s.Handle("/players/add", "add_player.html", handleAddPlayer).Methods("GET", "POST")
 	s.Handle("/", "index.html", handleIndex).Methods("GET")
